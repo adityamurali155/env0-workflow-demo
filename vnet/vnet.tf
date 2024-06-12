4f4a6638fd0bd73f9020519b18800620d096be5d
adityamurali155 <adityamurali2@gmail.com>
2024-06-12 18:30:07 +0530

    modified vnet

--- vnet/vnet.tf
+++ vnet/vnet.tf
@@ -10,12 +10,12 @@ resource "azurerm_virtual_network" "az-vnet" {
   name                = "AzureVnet"
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.30.0.0/16"]
 }
 
 resource "azurerm_subnet" "az-subnet" {
   name                 = "AzureSubnet"
   virtual_network_name = azurerm_virtual_network.az-vnet.name
   resource_group_name  = azurerm_resource_group.main.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.30.1.0/24"]
 }
