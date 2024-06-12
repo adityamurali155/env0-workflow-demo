60c9e31dea039365b204a015563f484eb3c3ef50
adityamurali155 <adityamurali2@gmail.com>
2024-06-12 19:22:53 +0530

    failed deployment vnet

--- vnet/vnet.tf
+++ vnet/vnet.tf
@@ -17,5 +17,5 @@ resource "azurerm_subnet" "az-subnet" {
   name                 = "AzureSubnet"
   virtual_network_name = azurerm_virtual_network.az-vnet.name
   resource_group_name  = azurerm_resource_group.main.name
-  address_prefixes     = ["10.30.1.0/24"]
+  address_prefixes     = ["10.30.1.0/16"]
 }
