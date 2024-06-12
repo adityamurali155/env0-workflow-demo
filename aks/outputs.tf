4e7b751cc76d7fca0bd799aa7c1e6fc6ba3e474a
adityamurali155 <adityamurali2@gmail.com>
2024-06-12 17:32:09 +0530

    modified aks and flux

--- aks/outputs.tf
+++ aks/outputs.tf
@@ -1,17 +1,19 @@
-output "fqdn" {
-  value     = azurerm_kubernetes_cluster.main.fqdn
-  sensitive = true
-}
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+output "host" {
+  value     = module.cluster.cluster_fqdn
   sensitive = true
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.main.kube_config_raw
+output "client_certificate" {
+  value     = module.cluster.client_certificate
   sensitive = true
 }
+
 output "client_key" {
-  value     = azurerm_kubernetes_cluster.main.client_key
+  value     = module.cluster.client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = module.cluster.cluster_ca_certificate
   sensitive = true
 }
